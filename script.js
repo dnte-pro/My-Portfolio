5886b50cbf78d46a99de5271c856513445fd9b5f
dnte-pro <kipronoyegon50@hmail.com>
2025-09-07 15:49:17 +0300

    Final commits

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 }
  
+
+function sendMail() {
+    let parms = {
+        name: document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        number: document.getElementById("number").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value
+    };
+    emailjs.send("service_yl2w5fy", "template_12iqqka", parms)
+        .then(function(response) {
+            alert("Message Sent Successfully");
+        })
+        .catch(function(error) {
+            alert("Failed to send message: " + error.text);
+        });
+}
